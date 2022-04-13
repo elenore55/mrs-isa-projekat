d9a26639a8dc3e131cacbb6b0908a0fb7c1b253a
SimicAleksa <aleksadsimic@gmail.com>
2022-04-13 09:24:01 +0200

    #5.1 Forma za unos podataka o avanturi

--- src/main/resources/static/app/app_addAdventure.js
+++ src/main/resources/static/app/app_addAdventure.js
@@ -1,6 +1,12 @@
 new Vue({
    el: '#app_addAdventure',       // imamo kontrolu nad divom i sad mozemo da saljemo odnosno prikazujemo podatke na njeme
    data:{
-      title:'AddAdventure'
+      title:'AddAdventure',
+      style_addAdventure:''
+   },
+   methods:{
+      changeTitle(){
+         this.title='ChangedAgain';
+      }
    }
 });
