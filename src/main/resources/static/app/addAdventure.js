2e2bd14b2559830566b4f5cafe4233cadae74fa8
SimicAleksa <aleksadsimic@gmail.com>
2022-04-22 19:12:47 +0200

    #5.2 Validacija unesenih podataka

--- src/main/resources/static/app/addAdventure.js
+++ src/main/resources/static/app/addAdventure.js
@@ -25,10 +25,10 @@ Vue.component("add-adventure",{
     },
     mounted: function (){
         this.loadEquipment()
-        this.loadInstructorBio_data()
+        // this.loadInstructorBio_data()
     },
     template: `
-    <form style="width: 1000px; margin: auto">
+    <form style="width: 1000px; margin: auto" v-on:submit="sendRequest">
         <h2 class="text-center">Add Adventure</h2>
         <div class="row">
             <div class="col">
@@ -53,15 +53,15 @@ Vue.component("add-adventure",{
             <div class="col">
                 <div class="form-group">
                     <label>Country</label>
-                    <input v-model="form.address.country" type="text" class="form-control"">
+                    <input v-model="form.address.country" type="text" class="form-control" pattern="([A-Z])([A-Za-z]+)$">
                 </div>
                 <div class="form-group">
                     <label>City</label>
-                    <input v-model="form.address.city" type="text" class="form-control"">
+                    <input v-model="form.address.city" type="text" class="form-control" pattern="([A-Z])([A-Za-z]+)$">
                 </div>
                 <div class="form-group">
                     <label>Street</label>
-                    <input v-model="form.address.street" type="text" class="form-control"">
+                    <input v-model="form.address.street" type="text" class="form-control">
                 </div>
             </div>
             <div class="col-2">
@@ -94,7 +94,7 @@ Vue.component("add-adventure",{
             <div class="col-3">
                 <div class="form-group">
                         <label>Maximum number of people</label>
-                        <input v-model="form.maxPeople" class="form-control" style="width: 100px">
+                        <input v-model="form.maxPeople" class="form-control" style="width: 100px" pattern="[0-9]{0,4}">
                 </div>
             </div>
         </div>
@@ -108,7 +108,7 @@ Vue.component("add-adventure",{
                 <div class="form-group">
                    <br>
 <!--                   <input type="button" class="btn btn-primary form-control" value="Submit">-->
-                   <button type="button" class="btn btn-primary btn-lg" v-on:click="sendRequest" style="position: relative; right: -220px;bottom: 0px">Submit</button>
+                   <button type="submit" class="btn btn-primary btn-lg" style="position: relative; right: -220px;bottom: 0px">Submit</button>
                 </div>
             </div>
         </div>
