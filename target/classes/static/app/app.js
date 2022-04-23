b769dd3cad79785d22e6e0de0e84e025d7e8f1b3
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-04-23 21:16:23 +0200

    Nesto iskonfigurisano

--- target/classes/static/app/app.js
+++ target/classes/static/app/app.js
@@ -1,12 +1,14 @@
+const ProfilePageInstructorPI = {template: '<profile-page-instructorpi></profile-page-instructorpi>'};
+const AddAdventure = {template: '<add-adventure></add-adventure>'};
 const CottageImages = {template: '<cottage-images></cottage-images>'};
 const UpdateCottageNav = {template: '<update-cottage-nav></update-cottage-nav>'};
 const UpdateCottage = {template: '<update-cottage></update-cottage>'};
 const AddCottage = {template: '<add-cottage></add-cottage>'};
 const Login = {template: '<login></login>'};
-const Registration = {template: '<registration></registration>'}
-const ClientNavbar = {template: '<client-navbar></client-navbar>'}
-const ClientProfile = {template: '<client-profile></client-profile>'}
-const EditProfile = {template: '<edit-profile></edit-profile>'}
+const Registration = {template: '<registration></registration>'};
+const ClientNavbar = {template: '<client-navbar></client-navbar>'};
+const ClientProfile = {template: '<client-profile></client-profile>'};
+const EditProfile = {template: '<edit-profile></edit-profile>'};
 
 const router = new VueRouter({
     mode: 'hash',
@@ -46,6 +48,14 @@ const router = new VueRouter({
         {
             path: "/addCottage/",
             component: AddCottage
+        },
+        {
+           path: "/addAdventure/",
+           component: AddAdventure
+        },
+        {
+           path: "/profilePageInstructorPI/",
+           component: ProfilePageInstructorPI
         }
 
 
