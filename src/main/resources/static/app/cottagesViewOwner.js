698127759be8903ebe3ceac7aa96d0388c1ad9c2
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-04-28 12:37:53 +0200

    #250.2 Dodavanje linkova za azuriranje, brisanje i detaljniji prikaz

--- src/main/resources/static/app/cottagesViewOwner.js
+++ src/main/resources/static/app/cottagesViewOwner.js
@@ -18,16 +18,30 @@ Vue.component("cottages-view-owner", {
             <div v-for="(c, i) in cottages" class="container card m-3">
                 <div class="row">
                     <div class="col-3">
-                        <img src="https://picsum.photos/id/81/200/300" class="card-img" width="250" height="250"  alt="cottage image">
+                        <img src="https://picsum.photos/id/81/200/300" class="card-img" width="200" height="200"  alt="cottage image">
                     </div>
-                    <div class="col-7">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ c.name }}</h5>
-                            <p class="card-text">{{ c.description }}</p>
+                    <div class="col-7 card-body container">
+                        <h5 class="card-title">{{ c.name }}</h5>
+                        <p class="card-text mt-3">{{ c.address.street }}, {{ c.address.city }}, {{ c.address.country }}</p>
+                        <p class="card-text">Price: {{ c.price }} EUR</p>
+                        <div class="d-flex flex-row mt-3">
+                            <a href="/#/updateCottage" class="btn btn-primary me-3 mt-2">View</a>
+                            <button v-on:click="deleteCottage" class="btn btn-danger mt-2">Delete</button>
                         </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p class="m-3">{{ c.description }}</p>
                     </div>
                 </div>
             </div>
         </div>
-    `
+    `,
+
+    methods: {
+        deleteCottage() {
+
+        }
+    }
 });
