db497358a2bb380efb886b49a8ae3656c2c022b5
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-04-28 12:03:21 +0200

    Izlistavanje kartica #250.1

--- src/main/resources/static/app/cottagesViewOwner.js
+++ src/main/resources/static/app/cottagesViewOwner.js
@@ -8,13 +8,26 @@ Vue.component("cottages-view-owner", {
     mounted() {
         axios.get("api/cottageOwner/getCottages/2").then(response => {
             this.cottages = response.data;
-            alert(this.cottages);
         }).catch(function (error) {
             alert('An error occurred!');
         });
     },
 
     template: `
-        <div>VIEW</div>
+        <div class="container">
+            <div v-for="(c, i) in cottages" class="container card m-3">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="https://picsum.photos/id/81/200/300" class="card-img" width="250" height="250"  alt="cottage image">
+                    </div>
+                    <div class="col-7">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ c.name }}</h5>
+                            <p class="card-text">{{ c.description }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     `
 });
