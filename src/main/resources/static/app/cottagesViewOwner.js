ca39b4ff5040a302553fde0c95c376d504d131cd
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-04-28 12:57:52 +0200

    #250.1 Izlistavanje kartica sa osnovnim podacima o vikendici

--- src/main/resources/static/app/cottagesViewOwner.js
+++ src/main/resources/static/app/cottagesViewOwner.js
@@ -17,16 +17,16 @@ Vue.component("cottages-view-owner", {
         <div class="container">
             <div v-for="(c, i) in cottages" class="container card m-3">
                 <div class="row">
-                    <div class="col-3">
-                        <img src="https://picsum.photos/id/81/200/300" class="card-img" width="200" height="200"  alt="cottage image">
+                    <div class="col-3 mb-2">
+                        <img src="https://picsum.photos/id/81/200/300" class="card-img rounded-3 mt-3" width="200" height="200"  alt="cottage image">
                     </div>
-                    <div class="col-7 card-body container">
+                    <div class="col-4 card-body container">
                         <h5 class="card-title">{{ c.name }}</h5>
                         <p class="card-text mt-3">{{ c.address.street }}, {{ c.address.city }}, {{ c.address.country }}</p>
                         <p class="card-text">Price: {{ c.price }} EUR</p>
                         <div class="d-flex flex-row mt-3">
-                            <a href="/#/updateCottage" class="btn btn-primary me-3 mt-2">View</a>
-                            <button v-on:click="deleteCottage" class="btn btn-danger mt-2">Delete</button>
+                            <a href="/#/updateCottage" class="btn btn-primary me-3 mt-3">View</a>
+                            <button v-on:click="deleteCottage" class="btn btn-danger mt-3">Delete</button>
                         </div>
                     </div>
                 </div>
