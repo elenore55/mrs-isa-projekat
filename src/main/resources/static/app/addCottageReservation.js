fd9147e8151c2752f4627d5dce9e274616e8f7dc
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-05-09 23:16:12 +0200

    #212.2 Validacija podataka o rezervaciji

--- src/main/resources/static/app/addCottageReservation.js
+++ src/main/resources/static/app/addCottageReservation.js
@@ -4,7 +4,8 @@ Vue.component('add-cottage-reservation', {
             cottage_id: null,
             start: null, end: null,
             name: "",
-            email: ''
+            email: '',
+            input_started: false
         }
     },
 
@@ -27,30 +28,50 @@ Vue.component('add-cottage-reservation', {
                     <h5>Reservation for {{ name }}</h5>
                 </div>
                 <div class="card-body">
-                    <div class="row m-3">
+                    <div class="row mt-3 ms-3 me-3 mb-1">
                         <div class="col form-floating">
                             <input type="email" id="client-mail-input" v-model="email" class="form-control">
                             <label for="client-mail-input">Client email</label>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-around m-3">
-                        <div class="mt-1 ms-3 me-4 mb-3">
+                    <p v-if="!isValidEmail" class="ms-4 text-danger">Client's email is required!</p>
+                    <div class="d-flex justify-content-around ms-3 mt-4 me-3 mb-1">
+                        <div class="mt-1 ms-3 me-4 mb-1">
                             <label for="start-date">Start date</label>
-                            <vuejs-datepicker v-model="start" format="dd.MM." id="start-date"></vuejs-datepicker>
+                            <vuejs-datepicker v-model="start" format="dd.MM.yyyy." id="start-date"></vuejs-datepicker>
                         </div>
-                        <div class="mt-1 mb-3 me-3">
+                        <div class="mt-1 mb-1 me-3">
                             <label for="end-date">End date</label>                
-                            <vuejs-datepicker v-model="end" format="dd.MM." id="end-date"></vuejs-datepicker>
+                            <vuejs-datepicker v-model="end" format="dd.MM.yyyy." id="end-date"></vuejs-datepicker>
                         </div>
                     </div>
-                    <div class="row mt-1 mb-3 me-3">
+                    <p v-if="!areValidDates" class="ms-4 text-danger">Dates are required and must be in ascending order!</p>
+                    <div class="row mt-4 mb-3 me-3">
                         <div class="col text-end">
-                            <button type="button" class="btn btn-primary">Submit</button>
+                            <button type="button" class="btn btn-primary" v-on:click="addReservation">Submit</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    `
+    `,
+
+    methods: {
+        addReservation() {
+            this.input_started = true;
+        }
+    },
+
+    computed: {
+        areValidDates() {
+            if (!this.input_started) return true;
+            return !!(this.start && this.end && this.cottage.availableStart < this.cottage.availableEnd);
+        },
+
+        isValidEmail() {
+            if (!this.input_started) return true;
+            return !!(this.email);
+        }
+    }
 });
