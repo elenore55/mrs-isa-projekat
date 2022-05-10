ebf3811e99a292194e3193cea58d91a78ede4935
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-05-10 23:10:54 +0200

    #202.2 Validacija podataka o akciji

--- src/main/resources/static/app/fastReservations.js
+++ src/main/resources/static/app/fastReservations.js
@@ -4,7 +4,7 @@ Vue.component('fast-reservations', {
             id: null,
             actions: [],
             start: null, duration: null, actionStart: null, actionDuration: null,
-            price: null, maxPeople: null
+            price: null, maxPeople: null, input_started: false
         }
     },
 
@@ -32,17 +32,19 @@ Vue.component('fast-reservations', {
                     <button type="button" v-on:click="actions.splice(i, 1)" class="btn btn-outline-danger">Delete</button>
                 </div>
             </div>
-            <div class="card position-fixed bottom-0 end-0 mb-5">
+            <div class="card position-fixed bottom-0 end-0 mb-2">
                 <div class="card-body">
                     <h5 class="ms-3 mt-2">Stay period</h5>
                     <div class="d-flex justify-content-left ms-3 mt-4 me-3 mb-1">
                         <div class="mt-1 ms-3 me-4 mb-1">
                             <label for="start-date">Start date</label>
                             <vuejs-datepicker v-model="start" format="dd.MM.yyyy." id="start-date"></vuejs-datepicker>
+                            <p v-if="!isValidStayDate" class="text-danger">Date must not be empty</p>
                         </div>
                         <div class="mt-1 mb-1 me-3 form-floating">
                             <input id="duration-input" v-model="duration" type="number" class="form-control" />
                             <label for="duration-input">Duration (days)</label>  
+                            <p v-if="!isValidStayDuration" class="text-danger">Duration must be positive</p>
                         </div>
                     </div>
                     <hr>
@@ -51,10 +53,12 @@ Vue.component('fast-reservations', {
                         <div class="mt-1 ms-3 me-4 mb-1">
                             <label for="start-action-date">Start date</label>
                             <vuejs-datepicker v-model="actionStart" format="dd.MM.yyyy." id="start-action-date"></vuejs-datepicker>
+                            <p v-if="!isValidActionDate" class="text-danger">Date must not be empty</p>
                         </div>
                         <div class="mt-1 mb-1 me-3 form-floating">               
                             <input id="action-duration-input" v-model="actionDuration" type="number" class="form-control" />
                             <label for="action-duration-input">Duration (days)</label> 
+                            <p v-if="!isValidActionDuration" class="text-danger">Duration must be positive</p>
                         </div>
                     </div>
                     <hr>
@@ -62,14 +66,16 @@ Vue.component('fast-reservations', {
                         <div class="form-floating">
                             <input type="number" v-model="price" step="0.01" id="price-input" class="form-control">
                             <label for="price-input">Price</label>
+                            <p v-if="!isValidPrice" class="text-danger">Price must be positive</p>
                         </div>
                         <div class="form-floating">
                             <input type="number" v-model="maxPeople" id="max-people-input" class="form-control">
                             <label for="max-people-input">Max people</label>
+                            <p v-if="!isValidMaxPeople" class="text-danger">Number of people must be positive</p>
                         </div>
                     </div>
                     <div class="text-end">
-                        <button type="button" class="btn btn-primary">Add</button>
+                        <button type="button" class="btn btn-primary" v-on:click="addAction">Add</button>
                     </div>
                 </div>
             </div>
@@ -79,7 +85,44 @@ Vue.component('fast-reservations', {
 
     methods: {
         addAction() {
-            alert('Adding');
+            this.input_started = true;
+            if (this.isValidActionDate && this.isValidActionDuration && this.isValidPrice && this.isValidMaxPeople &&
+                this.isValidStayDate && this.isValidStayDuration) {
+                alert('Adding');
+            }
+        }
+    },
+
+    computed: {
+        isValidPrice() {
+            if (!this.input_started) return true;
+            return this.price > 0;
+        },
+
+        isValidMaxPeople() {
+            if (!this.input_started) return true;
+            return this.maxPeople > 0;
+        },
+
+        isValidStayDate() {
+            if (!this.input_started) return true;
+            return !!(this.start)
+        },
+
+        isValidActionDate() {
+            if (!this.input_started) return true;
+            return !!(this.actionStart)
+        },
+
+        isValidStayDuration() {
+            if (!this.input_started) return true;
+            return this.duration > 0;
+        },
+
+        isValidActionDuration() {
+            if (!this.input_started) return true;
+            return this.actionDuration > 0;
         }
     }
+
 });
