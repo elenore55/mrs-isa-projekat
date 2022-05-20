18a25cefeba64b229908b1fe5d10915ef2c183c8
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-05-20 14:59:56 +0200

    Iscrtavanje praznog kalendara

--- src/main/resources/static/app/reservationsCalendar.js
+++ src/main/resources/static/app/reservationsCalendar.js
@@ -1,13 +1,27 @@
 Vue.component("reservations-calendar", {
-   data() {
-       return {
+    data() {
+        return {
+            reservations: []
+        }
+    },
 
-       }
-   },
+    mounted() {
+        var el = document.getElementById("calendar");
+        var calendar = new FullCalendar.Calendar(el, {
+            plugins: ['dayGrid', 'bootstrap'],
+            themeSystem: 'bootstrap',
+            weekNumbers: true,
+            eventLimit: true,
+            events: 'https://fullcalendar.io/demo-events.json'
+        });
+        calendar.render();
+    },
 
-   template: `
-   <div>
-   Calendar
-   </div>
-   `
+    template: `
+    <div class="m-3">
+        <div id="calendar">
+       
+        </div>
+    </div>
+    `
 });
