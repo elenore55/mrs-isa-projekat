907facf1860063e0cf2f5ae8ad4eb6d92b4ff960
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-05-27 22:04:45 +0200

    Prikaz osnovnih podataka

--- src/main/resources/static/app/incomeReport.js
+++ src/main/resources/static/app/incomeReport.js
@@ -6,8 +6,8 @@ Vue.component('income-report', {
             reports: [],
             disabled: {
                 from: new Date()
-            }
-
+            },
+            chart_displayed: false
         }
     },
 
@@ -28,31 +28,69 @@ Vue.component('income-report', {
                 </div>
             </div>
         </div>    
-        <div class="d-flex justify-content-center" style="background-color: #ddc8fb">
-            <div class="w-50 d-flex justify-content-end">
-                 
-            </div>
+        <div class="container-fluid" v-if="chart_displayed">
+            <canvas id="bar-plot"></canvas>
         </div>
     </div>
     `,
 
     methods: {
         getReports() {
+            this.chart_displayed = true;
             axios.post("api/users/getIncomeReport/" + this.$route.params.id, {
                 start: this.start_date,
                 end: this.end_date
             }).then(response => {
                 this.reports = response.data;
-                alert(this.reports.length);
+                let chart = document.getElementById('bar-plot').getContext('2d');
+                let incomeChart = new Chart(chart, {
+                    type: 'bar',
+                    data: {
+                        labels: this.getChartLabels(),
+                        datasets: [{
+                            label: 'Income',
+                            data: this.getChartData(),
+                            /*backgroundColor: [
+                                '#ff0000',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                '#ff0000',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],*/
+                            borderWidth: 1,
+                            hoverBorderWidth: 3
+                        }]
+                    },
+                    options: {}
+                });
             }).catch(error => {
                 Swal.fire('Error', 'Something went wrong!', 'error');
             })
-        }
-    },
+        },
 
-    computed: {
-        today() {
-            return new Date();
+        getChartLabels() {
+            let labels = [];
+            for (const report of this.reports) {
+                labels.push(report.offerName);
+            }
+            return labels;
+        },
+
+        getChartData() {
+            let chartData = [];
+            for (const report of this.reports) {
+                chartData.push(report.income);
+            }
+            return chartData;
         }
     }
 });
