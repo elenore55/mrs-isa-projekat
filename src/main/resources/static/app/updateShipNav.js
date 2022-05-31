a80d6bfc4c3a79ac2299fad1cfac12e03734e1a6
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-05-31 18:14:34 +0200

    Popravljen navbar za brodove i vikendice

--- src/main/resources/static/app/updateShipNav.js
+++ src/main/resources/static/app/updateShipNav.js
@@ -1,22 +1,40 @@
 Vue.component('update-ship-nav', {
     template: `
         <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-            <div class="collapse navbar-collapse" id="navbar_ship">
+            <div class="collapse navbar-collapse">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="javascript:void(0)" @click="$router.push({path: '/shipsViewOwner/1'})">All</a>
+                        <a class="nav-link" :href="allShips">All ships</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="javascript:void(0)" @click="$router.push({path: '/updateShip/ + $route.params.id'})">Profile</a>
+                        <a class="nav-link" :href="shipProfile">Profile</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="javascript:void(0)" @click="$router.push({path: '/shipImages/' + $route.params.id})">Images</a>
+                        <a class="nav-link" :href="shipImages">Images</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="javascript:void(0)" @click="$router.push({path: '/shipReservations/' + $route.params.id})">Reservations</a>
+                        <a class="nav-link" :href="shipReservations">Reservations</a>
                     </li>
                 </ul>
             </div>
         </nav>
-    `
+    `,
+
+    computed: {
+        allShips() {
+            return "/#/shipsViewOwner/1";
+        },
+
+        shipProfile() {
+            return "/#/updateShip/" + this.$route.params.id;
+        },
+
+        shipImages() {
+            return "/#/shipImages/" + this.$route.params.id;
+        },
+
+        shipReservations() {
+            return "/#/shipReservations/" + this.$route.params.id;
+        }
+    }
 });
