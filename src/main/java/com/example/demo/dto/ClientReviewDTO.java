d5039b883169e32c1cfd0c768ae08411db3e5003
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-06-09 23:39:51 +0200

    Kreiran ClientReviewDTO

---
+++ src/main/java/com/example/demo/dto/ClientReviewDTO.java
@@ -0,0 +1,74 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.ClientReview;
+
+import java.time.LocalDateTime;
+
+public class ClientReviewDTO {
+    private Integer id;
+    private LocalDateTime dateTime;
+    private String content;
+    private String clientEmail;
+    private Integer ownerId;
+    private Boolean penaltyRequested;
+
+    public ClientReviewDTO() {
+    }
+
+    public ClientReviewDTO(ClientReview cr) {
+        this.id = cr.getId();
+        this.dateTime = cr.getDateTime();
+        this.content = cr.getContent();
+        this.clientEmail = cr.getClient().getEmail();
+        this.ownerId = cr.getIssuedBy().getId();
+        this.penaltyRequested = cr.getPenaltyRequested();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getClientEmail() {
+        return clientEmail;
+    }
+
+    public void setClientEmail(String clientEmail) {
+        this.clientEmail = clientEmail;
+    }
+
+    public Integer getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Integer ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Boolean getPenaltyRequested() {
+        return penaltyRequested;
+    }
+
+    public void setPenaltyRequested(Boolean penaltyRequested) {
+        this.penaltyRequested = penaltyRequested;
+    }
+}
---
+++ src/main/java/com/example/demo/model/ClientReview.java
@@ -0,0 +1,86 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class ClientReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "complaint_date_time")
+    private LocalDateTime dateTime;
+
+    @Column
+    private String content;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User issuedBy;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Client client;
+
+    @Column
+    private Boolean penaltyRequested;
+
+    public ClientReview() {
+    }
+
+    public ClientReview(LocalDateTime dateTime, String content, User issuedBy, Client client, Boolean penaltyRequested) {
+        this.dateTime = dateTime;
+        this.content = content;
+        this.issuedBy = issuedBy;
+        this.client = client;
+        this.penaltyRequested = penaltyRequested;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getIssuedBy() {
+        return issuedBy;
+    }
+
+    public void setIssuedBy(User issuedBy) {
+        this.issuedBy = issuedBy;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Boolean getPenaltyRequested() {
+        return penaltyRequested;
+    }
+
+    public void setPenaltyRequested(Boolean penaltyRequested) {
+        this.penaltyRequested = penaltyRequested;
+    }
+}
