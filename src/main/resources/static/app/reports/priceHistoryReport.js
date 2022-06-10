95760ed0dc20d86ed3f876498f98b0da37781582
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-06-10 09:35:35 +0200

    #236.1 Forma za unos vremenskog perioda

---
+++ src/main/resources/static/app/reports/priceHistoryReport.js
@@ -0,0 +1,48 @@
+Vue.component('price-history-report', {
+    data() {
+        return {
+            start_date: null,
+            end_date: null,
+            prices: [],
+            offer_type: "",
+            disabled: {
+                from: new Date()
+            },
+            ch: null,
+            colors: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
+            border_colors: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)']
+        }
+    },
+
+    template: `
+    <div>
+        <owners-nav :offer="offer_type"></owners-nav>
+        <div class="d-flex justify-content-center" style="background-color: #ddc8fb">
+            <div class="w-25 d-flex justify-content-evenly my-3">
+                <div class="mt-1 ms-3 me-4 mb-1">
+                    <label for="start-date">Start date</label>
+                    <vuejs-datepicker v-model="start_date" format="dd.MM.yyyy." id="start-date" :disabled-dates="disabled"></vuejs-datepicker>     
+                </div>
+                <div class="mt-1 ms-3 me-4 mb-1">
+                    <label for="end-date">End date</label>
+                    <vuejs-datepicker v-model="end_date" format="dd.MM.yyyy." id="end-date" :disabled-dates="disabled"></vuejs-datepicker>
+                </div>
+                <div class="d-flex align-items-end">
+                    <button class="btn btn-success px-3 mx-2 h-75 my-1" v-on:click="getReports">Display</button>
+                </div>
+            </div>
+        </div>    
+        <div class="container w-75">
+            <canvas id="bar-plot"></canvas>
+        </div>
+    </div>
+    `,
+
+    methods: {
+        getReports() {
+
+        }
+    }
+});
