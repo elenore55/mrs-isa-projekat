5a7b33b70b62c8143d2400d8ce7144a1315960fe
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2022-06-21 13:34:38 +0200

    Ispravljen prikaz slika

--- target/classes/static/app/shipImages.js
+++ target/classes/static/app/shipImages.js
@@ -28,9 +28,11 @@ Vue.component("ship-images", {
         <update-ship-nav></update-ship-nav>
         <div class="container px-2">
             <div class="row">
-                <div class="col d-flex justify-content-center flex-wrap" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <div class="col d-flex justify-content-center flex-wrap">
                     <div v-for="(img, i) in paths" class="m-2">
-                        <img :src="img" class="rounded float-start m-2" width="240" height="240" data-bs-target="#carouselExample" :data-bs-slide-to="i" style="cursor: pointer">
+                        <div data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <img :src="img" class="rounded float-start m-2" width="240" height="240" data-bs-target="#carouselExample" :data-bs-slide-to="i" style="cursor: pointer">
+                        </div>
                         <div class="text-end">
                             <button type="button" class="btn btn-sm btn-danger me-3" v-on:click="paths.splice(i, 1)">Delete</button>
                         </div>
